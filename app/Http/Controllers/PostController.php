36eb402ba7cc515d165d209bca9fab6058dae476
Edmond Hobeika <edmondhobeika@Edmonds-MacBook-Pro.local>
2017-03-01 19:51:07 +0000

    order table to display posts by latest post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
     public function index()
     {
         // create a variable and store all the blog posts fro the database
-        $posts = Post::paginate(10);
+        $posts = Post::orderBy('id', 'desc')->paginate(10);
         
         // return a view and pass in the variable
         return view('posts.index', [
