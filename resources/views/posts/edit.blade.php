7818be4e5ab42ee5b958b99624778b7ce3c743d2
Edmond Hobeika <edmondhobeika@Edmonds-MacBook-Pro.local>
2017-03-04 20:00:54 +0000

    outputting html data inserted by tinymce to html

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,6 +4,17 @@
 
 @section('stylesheets')
 	{!! Html::style('css/select2.min.css') !!}
+
+	<script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
+	
+	<script>
+		tinymce.init({
+			selector: 'textarea',
+			plugins: 'link code'
+		});		
+	</script>
+
+
 @endsection
 
 @section('content')
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,7 +27,7 @@
 						<tr>
 							<td>{{ $post->id }}</td>
 							<td>{{ $post->title }}</td>
-							<td>{{ substr($post->body,0,50) }}{{ strlen($post->body) > 50 ? "...." : " " }}</td>
+							<td>{{ substr(strip_tags($post->body),0,50) }}{{ strlen(strip_tags($post->body)) > 50 ? "...." : " " }}</td>
 							<td>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</td>
 							<td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>
 						</tr>
